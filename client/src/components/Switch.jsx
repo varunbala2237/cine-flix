d74b10534bec343766ab960a6f47c6fb77456fbf
Varun Bala <varunbala2237@users.noreply.github.com>
2025-12-22 14:58:07 +0530

    Final build client@0.1.0 3:00PM 22/12/2025 IST

--- client/src/components/Switch.jsx
+++ client/src/components/Switch.jsx
@@ -3,12 +3,12 @@ export default function Switch({ checked, onChange }) {
     <button
       type="button"
       onClick={() => onChange(!checked)}
-      className={`w-9 h-5 rounded-full relative transition ${
+      className={`w-9 h-5 relative transition ${
         checked ? "bg-yellow-500" : "bg-zinc-800"
       }`}
     >
       <span
-        className={`w-4 h-4 rounded-full absolute top-0.5 transition ${
+        className={`w-4 h-4 absolute top-0.5 transition ${
           checked ? "left-4 bg-black" : "left-1 bg-white"
         }`}
       />
